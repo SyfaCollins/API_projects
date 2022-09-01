d0a8b49a6e40b69bf31bb23e5d4e022c62d14f19
SyfaCollins <coniplexy@gmail.com>
2022-09-02 01:05:47 +0300

    travel advisor

--- Travel_Advisor/travel-advisor/src/App.jsx
+++ Travel_Advisor/travel-advisor/src/App.jsx
@@ -1,18 +1,27 @@
-import React from "react";
-import Header from "./components/Header";
-import Map from "./components/Map";
-import PlaceDetails from "./components/PlaceDetails";
+/** @format */
 
+import React from "react";
+import Header from "./components/Header/Header";
+import Map from "./components/Map/Map";
+import List from "./components/List/List";
+// import PlaceDetails from "./components/PlaceDetails/PlaceDetails";
+import { CssBaseline, Grid } from "@material-ui/core";
 
 function App() {
-    return ( 
-        <div>
-            <h1>Hello World</h1>
-            <Header />
-            <Map />
-            <PlaceDetails />
-        </div>
-     );
+  return (
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+    </>
+  );
 }
 
 export default App;
---
+++ Travel_Advisor/travel-advisor/src/components/Map/Map.jsx
@@ -0,0 +1,13 @@
+/** @format */
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography } from "@material-ui/core";
+function Map() {
+  return (
+    <>
+      <h1>Map</h1>
+    </>
+  );
+}
+
+export default Map;
