99b12fd256ee3ded016e4c974da60ff03258ba74
SyfaCollins <coniplexy@gmail.com>
2022-09-04 06:34:37 +0300

    added styles and maps

--- Travel_Advisor/travel-advisor/src/components/List/List.jsx
+++ Travel_Advisor/travel-advisor/src/components/List/List.jsx
@@ -1,10 +1,18 @@
 /** @format */
+import React from "react";
+import {CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select} from '@material-ui/core'
+
+import useStyles from './styles'
 
 function List() {
+  const classes = useStyles()
     return (
-      <>
-        <h1>List</h1>
-      </>
+      <div className={classes.container}>
+        <Typography variant='h4'>
+          Restaurant, Hotels & Attractions around you
+        </Typography>
+
+      </div>
     );
   }
   
---
+++ Travel_Advisor/travel-advisor/src/components/Map/styles.js
@@ -0,0 +1,29 @@
+/** @format */
+
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles(() => ({
+    // body:{
+    //     background:'red';
+    // },
+  paper: {
+    padding: "10px",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    width: "100px",
+  },
+  mapContainer: {
+    height: "85vh",
+    width: "100%",
+  },
+  markerContainer: {
+    position: "absolute",
+    transform: "translate(-50%, -50%)",
+    zIndex: 1,
+    "&:hover": { zIndex: 2 },
+  },
+  pointer: {
+    cursor: "pointer",
+  },
+}));
